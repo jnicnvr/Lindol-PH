854157e2c4cf9b954b377e2cd97531e939117800
Konics.dev <konics.dev@gmail.com>
2023-02-21 23:20:38 +0800

    setup express &node-cron-job

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const lindol = require('./integration/earthquake_api')
+const moment = require('moment')
+const cron = require('node-cron');
 
 
-app.listen(8001, ()=>{
-    console.log('Starting at port 8001')
+if(process.env.COM_LINDOL_PH_NODE_CRON_JOB_ENABLED === "enabled"){
+    console.log(`Node Cron Job Enabled: ${process.env.COM_LINDOL_PH_NODE_CRON_JOB_ENABLED}`)
+    console.log(`Node Cron Job Schedule: ${process.env.COM_LINDOL_PH_NODE_CRON_JOB_SCHEEDULE}`)
+
+    cron.schedule(process.env.COM_LINDOL_PH_NODE_CRON_JOB_SCHEEDULE, () => {
+        lindol.currentEarthquakeEvent(moment)
+      });
+}
+
+app.listen(process.env.COM_LINDOL_PH_PORT, ()=>{
+    console.log(`Starting at port ${process.env.COM_LINDOL_PH_PORT}`)
+    lindol.currentEarthquakeEvent(moment)
 })
